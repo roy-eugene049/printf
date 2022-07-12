bbd380ed0ec1bf90bddde1f8ca39ad8d7b4ba7a9
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-07-12 12:51:03 +0300

    Initial Commit

--- check_mod.c
+++ check_mod.c
@@ -1,1 +1,21 @@
+#include "main.h"
+/**
+ * check_mode - checks for a ('%') symbol
+ * @c: inputed character
+ *
+ * Return: state of the func
+ */
+int check_mode(char c)
+{
+	int i;
+	char arr[] = "csdib\n%";
 
+	for (i = 0; arr[i]; i++)
+	{
+		if (arr[i] == c)
+			return (1);
+		if (c == '\0')
+			return (1);
+	}
+	return (0);
+}
