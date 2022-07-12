1f1bce42ef5c5a05c4b640f724aa43814bcee15c
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-07-12 12:38:14 +0300

    Initial Commit

--- main.h
+++ main.h
@@ -1,1 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _strlen(const char *s);
+int write_int(int n, int *ptr);
+int bin(unsigned int n, int *ptr);
+int write_char(char c);
+int write_str(char *s);
+int check_mode(char c);
+void check_func(va_list, char c, int *num_char, int *i);
+
+#endif
