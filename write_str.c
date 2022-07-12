8315e5241df09dae207725821eb25735c629d06f
Agatha <agkaranja5@gmail.com>
2022-07-12 14:00:58 -0700

    Add function

--- write_str.c
+++ write_str.c
@@ -9,4 +9,14 @@ int write_str(char *s)
 {
 	int length;
 
+	if (s == (char *)0)
+	{
+		s = "(null)";
+		write(1, s, 6);
+		return (6);
+	}
+	length = _strlen(s);
+
+	write(1, s, length);
+	return (length);
 }
