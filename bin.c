9a7a225e9ab5c2e288aa5f53b6d59c48de564129
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-07-12 12:49:45 +0300

    Initial Commit

--- bin.c
+++ bin.c
@@ -1,1 +1,20 @@
+#include "main.h"
+/**
+ * bin - converts to binary
+ * @n: input
+ * @ptr: pointer to the count variable
+ * Return: No return value
+ */
+int bin(unsigned int n, int *ptr)
+{
+	char c;
 
+	(*ptr)++;
+	if (n / 2 > 0)
+	{
+		bin(n / 2, ptr);
+	}
+	c = (n % 2) + '0';
+	write(1, &c, 1);
+	return (*ptr);
+}
