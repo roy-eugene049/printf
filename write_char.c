0c2d4908d01fc5bc2e120d331637bf147caeb301
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-07-12 12:52:12 +0300

    Initial Commit

--- write_char.c
+++ write_char.c
@@ -1,1 +1,12 @@
-
+#include "main.h"
+/**
+ * write_char - writes a character to the stdout
+ * @c: character inputed
+ *
+ * Return: number of characters printed
+ */
+int write_char(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
