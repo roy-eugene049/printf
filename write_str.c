5e6533a7261bb89e30407ff965f0bc387e8517e7
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-07-12 12:53:40 +0300

    Initial Commit

--- write_str.c
+++ write_str.c
@@ -1,1 +1,22 @@
+#include "main.h"
+/**
+ * write_str - writes a string to the stdout
+ * @s: input string
+ *
+ * Return: lenght of integers printed
+ */
+int write_str(char *s)
+{
+	int length;
 
+	if (s == (char *)0)
+	{
+		s = "(null)";
+		write(1, s, 6);
+		return (6);
+	}
+	length = _strlen(s);
+
+	write(1, s, length);
+	return (length);
+}
