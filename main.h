f9c3d0f90be2188368d9f815a7c2a17ddc1a855a
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-07-12 13:09:18 +0300

    Update

--- main.h
+++ main.h
@@ -1,18 +1,31 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
-#include <limits.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _printf(const char *format, ...);
-int _strlen(const char *s);
-int write_int(int n, int *ptr);
-int bin(unsigned int n, int *ptr);
-int write_char(char c);
-int write_str(char *s);
-int check_mode(char c);
-void check_func(va_list, char c, int *num_char, int *i);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
 #endif
